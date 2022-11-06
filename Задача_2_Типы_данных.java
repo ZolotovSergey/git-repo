e5e669380820e00ee8a20f32ea07ed5d90f90cf3
ZolotovSergey <103629913+ZolotovSergey@users.noreply.github.com>
2022-11-06 13:45:55 +0300

    Add files via upload

---
+++ Задача_2_Типы_данных.java
@@ -0,0 +1,12 @@
+package com.max.idea;
+
+public class Задача_2_Типы_данных {
+    public static void main(String[] args) {
+        System.out.println("Задача_2_Типы_данных");
+        double num = 7.9;
+        byte num2 = 127;
+        System.out.println("Переменная с плавающей точкой с наибольшим диапазоном чисел. Присвойте ей произвольное дробное значение : " + "double = " + num);
+        System.out.println("Переменная целочисленного типа с наименьшим диапазоном чисел. Присвойте ей наибольшее возможное значение : "  + "byte = " + num2);
+
+    }
+}
